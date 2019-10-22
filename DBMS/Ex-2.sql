fdffe4bd6fd7cc564034fceb07289a4d535f3bfe
Raj Patel <31172829+raj-savaj@users.noreply.github.com>
2019-10-23 01:10:26 +0530

    Update Ex-2.sql

--- DBMS/Ex-2.sql
+++ DBMS/Ex-2.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Flights(fno INT PRIMARY KEY,flight_from VARCHAR(50),flight_to VARCHAR(30),distance NUMBER(10),Departs varchar2(10),arrives varchar2(10),price NUMBER(10,2));
 CREATE TABLE Aircraft(aid INT PRIMARY KEY,aname  VARCHAR(50), cruisingrange NUMBER(10));
-CREATE TABLE Employees(eid INT PRIMARY KEY, ename VARCHAR(50), salary int)
-CREATE TABLE Certified(eid int references Employees(eid),aid int  references Aircraft(aid))
+CREATE TABLE Employees(eid INT PRIMARY KEY, ename VARCHAR(50), salary int);
+CREATE TABLE Certified(eid int references Employees(eid),aid int  references Aircraft(aid));
 
 INSERT ALL
 INTO Flights VALUES (1,'Bangalore','Mangalore',360,'10:45:00','12:00:00',10000)
@@ -55,20 +55,20 @@ INTO certified  VALUES (1,789)
 SELECT * FROM dual;
 
 
-#Find the names of aircraft such that all pilots certified to operate them have salaries more than Rs.80000
-select a.aname from Aircraft a,Employees e,certified c where e.salary>80000 AND c.eid=e.eid AND c.aid=a.aid 
+--Find the names of aircraft such that all pilots certified to operate them have salaries more than Rs.80000
+select a.aname from Aircraft a,Employees e,certified c where e.salary>80000 AND c.eid=e.eid AND c.aid=a.aid;
 
 
 
-#Find the names of pilots whose salry is less than the price of the cheapest route from Bangalore to Frankfurt
+--Find the names of pilots whose salry is less than the price of the cheapest route from Bangalore to Frankfurt
 select distinct e.ename from Employees e,certified c where e.eid=c.eid and e.salary<(select min(price) from flights where flight_from='Bangalore' and flight_to='Frankfurt');
 
 
 
-#Find the names of pilots certified for some Boeing aircraft
-select e.ename from Employees e,certified c,Aircraft a where c.eid=e.eid  AND a.ANAME='Boeing' AND c.aid=a.aid
+--Find the names of pilots certified for some Boeing aircraft
+select e.ename from Employees e,certified c,Aircraft a where c.eid=e.eid  AND a.ANAME='Boeing' AND c.aid=a.aid;
 
 
 
-#Find the aids of all aircraft that can be used on routes from Bengaluru to New Delhi.
+--Find the aids of all aircraft that can be used on routes from Bengaluru to New Delhi.
 select aid from Aircraft a where cruisingrange>(select max(distance) from Flights where flight_from='Bangalore' and flight_to='Delhi');
