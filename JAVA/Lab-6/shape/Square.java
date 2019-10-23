84f7189aadacc053dab032e55e5bb6bb5b9dadb6
Raj Patel <31172829+raj-savaj@users.noreply.github.com>
2019-10-23 18:59:55 +0530

    Create Square.java

---
+++ JAVA/Lab-6/shape/Square.java
@@ -0,0 +1,15 @@
+public class Square {
+	private int side;
+    public Square(int s)
+    {
+              side=s;
+    }
+    public int perimeter()
+    {
+              return (4*side);
+    }
+    public int area()
+    {
+              return (side*side);
+    }
+}
