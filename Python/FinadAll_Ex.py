9bb1e4b19f178f746920707e22a14013ee9a3710
Raj Patel <rsavaj7@gmail.com>
2020-03-05 09:52:38 +0530

    Findall(),search()

--- Python/FinadAll_Ex.py
+++ Python/FinadAll_Ex.py
@@ -1,7 +1,14 @@
 import re
 
 str="The rain in Spain"
-x = re.findall("ds",str)
+##x = re.findall("ai",str)
+##
+##print(x)
+#return the List if not match return empty
 
-print(x)
-#return the List
+# ~ The Search() Function
+x = re.search("S",str,re.I);
+if x :
+     print("Pattern Found")
+else:
+     print("Not Found")
