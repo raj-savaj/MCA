1630db49ff9e6d89c9da2c0238d1226bc6dae2c0
Raj Patel <rsavaj7@gmail.com>
2020-04-27 14:28:10 +0530

    Update LAB7.C

--- ADAA/LAB7.C
+++ ADAA/LAB7.C
@@ -1,1 +1,54 @@
-#include<
+#include<stdio.h>
+
+#include<conio.h>
+
+int min(int a, int b) {
+  return (a < b ? a : b);
+}
+void floyd();
+int n, cost[10][10], dist[10][10];
+
+void main() 
+{
+  int i, j, k;
+  clrscr();
+  printf("Enter number of nodes of the graph:");
+  scanf("%d", & n);
+  printf("\nEnter the adjacency matrix for the graph:\n");
+  for (i = 1; i <= n; i++){
+  	for (j = 1; j <= n; j++)
+  	{
+      scanf("%d", & cost[i][j]);
+  	}
+  }
+  printf("\n The adjacency matrix is:\n");
+  for (i = 1; i <= n; i++) 
+  {
+    for (j = 1; j <= n; j++) 
+    {
+      printf("%5d", cost[i][j]);
+      dist[i][j] = cost[i][j];
+    }
+    printf("\n");
+  }
+  floyd();
+  getch();
+}
+void floyd() {
+  int i, j, k;
+  for (k = 1; k <= n; k++){
+  	for (i = 1; i <= n; i++){
+  		for (j = 1; j <= n; j++){
+  			dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+  		}
+   	}
+  }
+    
+  printf("\n The Distance matrix is:\n");
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++){
+      printf("%5d", dist[i][j]);
+    }
+    printf("\n");
+  }
+}
