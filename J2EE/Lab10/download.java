e8cdd9caf08574abf96f334c8866364631858883
Raj Patel <rsavaj7@gmail.com>
2020-05-18 19:36:30 +0530

    Add files via upload

---
+++ J2EE/Lab10/download.java
@@ -0,0 +1,24 @@
+package Lab10;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/download")
+public class download extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("<html><body> ");
+		out.print("<a href=Lab10/download.html>Click</a>");
+		out.print("</body></html>");
+
+	}
+
+}
