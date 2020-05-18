546a2c2f4aa305ce2ed48d0eadbee7cf579715ec
Raj Patel <rsavaj7@gmail.com>
2020-05-18 19:27:19 +0530

    Add files via upload

---
+++ J2EE/Lab8/targetservlet.java
@@ -0,0 +1,23 @@
+package Lab8;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/targetservlet")
+public class targetservlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("<html><body><p>Hi Welcome to Servlet 2.</p>");
+		out.print("</body></html>");
+
+	}
+}
