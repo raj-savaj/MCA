154f257457db90b3f060adb39a636666ed6b0b0e
Raj Patel <rsavaj7@gmail.com>
2021-01-22 18:28:27 +0530

    Add files via upload

---
+++ Lab3.java
@@ -0,0 +1,15 @@
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Lab3 {
+	
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","H://MCA//chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.google.com/");
+        Thread.sleep(500);
+        driver.close();
+	}
+}
